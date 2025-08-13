b05c993df7e36341cbd4c89003867afb9122b45a
Katie Kloss <kloss.katherine@gmail.com>
2025-08-13 19:46:02 -0400

    ha_writer: report leader and lap number.

--- ha_writer.py
+++ ha_writer.py
@@ -5,19 +5,35 @@ from homeassistant_api import Client as HomeAssistantClient, State
 from pitwall.events import SessionStatus
 from pitwall.adapters import CaptureAdapter
 from pitwall import PitWallClient
+from pitwall.events.session import SessionProgress
+from pitwall.util.timing_tower import TimingLine, TimingTower
 
-ha: HomeAssistantClient
+ha: HomeAssistantClient = None
+session_status: SessionStatus = None
 
 async def main():
     global ha
     client = PitWallClient(CaptureAdapter("-"))
     client.on_session_status(on_session_status)
+    client.on_session_progress(on_session_progress)
+
+    timing = TimingTower(client)
+    timing.on_position_change(on_position_change)
 
     with HomeAssistantClient(os.environ["HOMEASSISTANT_URL"], os.environ["HOMEASSISTANT_TOKEN"]) as ha:
         await client.go()
 
 def on_session_status(update: SessionStatus):
+    global session_status
+    session_status = update
     ha.set_state(State(entity_id="sensor.f1_session", state=update.status))
+
+def on_session_progress(update: SessionProgress):
+    ha.set_state(State(entity_id="sensor.f1_session", state=session_status.status if session_status else "Unknown", attributes={"lap": update.lap}))
+
+def on_position_change(driver: TimingLine):
+    if driver.position == 1:
+        ha.set_state(State(entity_id="sensor.f1_leader", state=driver.name, attributes={"number": driver.driver_number}))
 
 if __name__ == "__main__":
     try:
--- src/pitwall/util/timing_tower.py
+++ src/pitwall/util/timing_tower.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Dict, List
+from typing import Callable, Dict, List
 from colorist import Color
 from pitwall.client import PitWallClient
 from pitwall.events import Driver, DriverPositionUpdate
@@ -24,6 +24,7 @@ class TimingTower:
     _client: PitWallClient
     drivers: Dict[int, TimingLine]
     results: List[TimingLine]
+    _on_position_change_callbacks: List[Callable[[TimingLine], None]]
 
     def __init__(self, client: PitWallClient):
         self._client = client
@@ -31,6 +32,10 @@ class TimingTower:
         self._client.on_driver_position_update(self._on_driver_position_update)
         self._client.on_timing_datum(self._on_timing_datum)
         self.drivers = dict()
+        self._on_position_change_callbacks = list()
+
+    def on_position_change(self, callback: Callable[[TimingLine], None]):
+        self._on_position_change_callbacks.append(callback)
 
     def _on_driver_data(self, data: List[Driver]):
         if len(self.drivers) > 0:
@@ -38,6 +43,10 @@ class TimingTower:
         
         for driver in data:
             self.drivers[driver.number] = TimingLine(driver.number, driver.broadcast_name, 99, 0, 0, 0)
+
+    def _call_position_update_callbacks(self, line: TimingLine):
+        for callback in self._on_position_change_callbacks:
+            callback(line)
 
     def _on_driver_position_update(self, update: DriverPositionUpdate):
         driver = self.drivers[update.driver_id]
@@ -56,18 +65,23 @@ class TimingTower:
 
             print(f"{Color.MAGENTA}\t{driver} {"overtook" if driver.position > update.position else "lost position to"} {swap_with}{Color.OFF}")
             swap_with.position = driver.position
+            self._call_position_update_callbacks(swap_with)
         elif driver.position > update.position: # overtake
             losses = [x for x in self.drivers.values() if update.position <= x.position < driver.position]
             for d in losses:
                 print(f"{Color.MAGENTA}\t{driver} overtook {d}{Color.OFF}")
                 d.position += 1
+                self._call_position_update_callbacks(d)
+
         elif driver.position < update.position:
             gains = [x for x in self.drivers.values() if driver.position < x.position <= update.position]
             for d in gains:
                 print(f"{Color.MAGENTA}\t{driver} lost position to {d}{Color.OFF}")
                 d.position -= 1
+                self._call_position_update_callbacks(d)
 
         driver.position = update.position
+        self._call_position_update_callbacks(driver)
         self.results = list(sorted(self.drivers.values(), key=lambda d: d.position))
     
     def _on_timing_datum(self, datum: TimingDatum):
