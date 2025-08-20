43ae4e599a5dd946d655fb762cd39bab5c6e91c7
Katie Kloss <kloss.katherine@gmail.com>
2025-08-20 06:14:46 -0400

    rabbit_replay: replay.py for rabbit.
    Moves the bulk of replay.py into a reusable class as well.

--- replay.py
+++ replay.py
@@ -1,11 +1,51 @@
 #!/usr/bin/env python
 
 import sys
-from anyio import open_file, run
+from anyio import run
 import asyncio
 import os
 import time
 import argparse
+import logging
+
+import orjson
+from pitwall.adapters.abstract import PitWallAdapter, Update
+
+class RealtimeReplayAdapter(PitWallAdapter):
+    _filename: str
+    _multiplier: int
+    _log: logging.Logger
+
+    def __init__(self, filename, multiplier = 1):
+        super().__init__()
+        self._filename = filename
+        self._multiplier = multiplier
+        self._log = logging.getLogger(__name__)
+
+    async def run(self):
+        with open(self._filename) as file:
+            last_ts = None
+            for next_line in file:
+                (ts, type, data) = next_line.split(":", 2)
+                ts = int(ts)
+                data = orjson.loads(data.rstrip())
+                if last_ts is None:
+                    ...
+                else:
+                    wait_s = (ts - last_ts) / 1000000000
+                    # helps get through waiting forever
+                    if wait_s > 5:
+                        wait_s = 5
+                    wait_s = wait_s / self._multiplier
+                    self._log.debug("Need to wait %.3fs for next update", wait_s)
+                    await asyncio.sleep(wait_s)
+
+                offset_time = time.time_ns()
+                self._message(Update(type, data, ts))
+                offset_time = time.time_ns() - offset_time
+                if offset_time < 0:
+                    offset_time = 0
+                last_ts = ts + offset_time
 
 async def main():
     if args.output is not None:
@@ -17,51 +57,29 @@ async def main():
     else:
         output = sys.stdout
 
-    async with await open_file(args.input) as file:
-        last_ts = None
-        async for next_line in file:
-            (ts, type, data) = next_line.split(":", 2)
-            ts = int(ts)
-            data = data.rstrip()
-            if last_ts is None:
-                ...
-            else:
-                wait_s = (ts - last_ts) / 1000000000
-                # helps get through waiting forever
-                if wait_s > 5:
-                    wait_s = 5
-                wait_s = wait_s / args.multiplier
-                await asyncio.sleep(wait_s)
+    adapter = RealtimeReplayAdapter(args.input, args.multiplier)
+    adapter.on_message(lambda u: output.write(f"{u.ts}:{u.src}:{orjson.dumps(u.data).decode()}\n"))
+    await adapter.run()
 
-            offset_time = time.time_ns()
-            output.write(f"{ts}:{type}:{data}\n")
-            output.flush()
-            offset_time = time.time_ns() - offset_time
-            if offset_time < 0:
-                offset_time = 0
-            last_ts = ts + offset_time
-
-    if output is not None:
-        output.close()
-
-try:
-    global args
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", required=True)
-    parser.add_argument("-o", "--output")
-    parser.add_argument("-x", "--multiplier")
-    args = parser.parse_args()
-    if args.multiplier is not None:
-        args.multiplier = int(args.multiplier)
-    else:
-        args.multiplier = 1
-
-    run(main)
-except (KeyboardInterrupt,BrokenPipeError):
-    ...
-finally:
+if __name__ == "__main__":
     try:
-        if args.output is not None:
-            os.remove(args.output)
-    except (NameError,FileNotFoundError):
+        global args
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-i", "--input", required=True)
+        parser.add_argument("-o", "--output")
+        parser.add_argument("-x", "--multiplier")
+        args = parser.parse_args()
+        if args.multiplier is not None:
+            args.multiplier = int(args.multiplier)
+        else:
+            args.multiplier = 1
+
+        run(main)
+    except (KeyboardInterrupt,BrokenPipeError):
         ...
+    finally:
+        try:
+            if args.output is not None:
+                os.remove(args.output)
+        except (NameError,FileNotFoundError):
+            ...
